6ee5ce64650b82646fbbc5ed1b104d0436bdc83c
kforkaran <karangupta199920@gmail.com>
2020-05-26 16:53:09 +0530

    InsufficientCapabilitiesException fixed while updating stack

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 aws cloudformation update-stack \
 --stack-name $1 \
---template-body file://$2 \
+--template-body file://$2  \
 --parameters file://$3 \
---region=us-west-2
---capabilities CAPABILITY_NAMED_IAM
+--region=us-west-2 \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
